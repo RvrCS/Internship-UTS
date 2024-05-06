e843e908675ff90379852da06e54ff03087bcafa
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2024-05-06 13:48:41 -0700

    Update

--- front/src/app/views/app/vien/start/start.component.ts
+++ front/src/app/views/app/vien/start/start.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Constantes } from 'src/app/constants/constantes';
 import { Facade } from 'src/app/shared/services/facadeService';
+import { ECharts, init } from 'echarts';
+
 export class CxCDashBoard {
   Fecha: string = '';
   NoFacturas: number = 0;
@@ -11,12 +13,43 @@ export class CxCDashBoard {
   templateUrl: './start.component.html'
 })
 export class StartComponent implements OnInit {
+  chart: ECharts;
 
   constructor(private facade: Facade) { }
 
-  async ngOnInit() {
+  
+  ngOnInit(): void {
+    this.chart = init(document.getElementById('IdGrafica') as HTMLDivElement);
+    const option = {
+      title: {
+        text: 'Ventas de los últimos 12 meses',
+        subtext: 'Ventas Acumuladas'
+      },
+      tooltip: {
+        trigger: 'axis',
+        formatter: '{a} : {b}'
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Sucursal A',
+          type: 'line',
+          data: [120000, 150000, 180000, 200000, 220000, 210000, 240000, 230000, 220000, 210000, 200000, 230000]
+        },
+        {
+          name: 'Otra línea',
+          type: 'line',
+          data: [100000, 130000, 160000, 190000, 200000, 190000, 220000, 210000, 200000, 190000, 180000, 210000]
+        }
+      ]
+    };
+    this.chart.setOption(option);
   }
-
-
-
+  
 }
